5c57936abc612e726cbbe8d8c97177201c8e6cfc
mojewski <mateusz.ojewski1@gmail.com>
2026-08-11 22:23:47 +0200

    Initial commit

---
+++ src/main/java/com/github/mojewski/footballleaguesimulator/FootballLeagueSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.github.mojewski.footballleaguesimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballLeagueSimulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballLeagueSimulatorApplication.class, args);
+    }
+
+}
