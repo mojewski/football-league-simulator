4a8049782cd2119c0023c3b6780612e83110404b
mojewski <293508@student.pwr.edu.pl>
2026-08-13 21:31:06 +0200

    Add some code to classes Player and Team. Add Position enum.

--- src/main/java/com/github/mojewski/footballleaguesimulator/model/Player.java
+++ src/main/java/com/github/mojewski/footballleaguesimulator/model/Player.java
@@ -1,4 +1,34 @@
 package com.github.mojewski.footballleaguesimulator.model;
 
 public class Player {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private int potential;
+    private int rating;
+    private Country country;
+    private Position position;
+    private int age;
+    private boolean isForSale;
+
+    public Player(String firstName, String lastName, int potential,
+                  int rating, Country country, Position position, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.potential = potential;
+        this.rating = rating;
+        this.country = country;
+        this.position = position;
+        this.age = age;
+    }
+
+    public Long getId() { return id; }
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+    public int getPotential() { return potential; }
+    public int getRating() { return rating; }
+    public Country getCountry() { return country; }
+    public Position getPosition() { return position; }
+    public int getAge() { return age; }
 }
--- src/main/java/com/github/mojewski/footballleaguesimulator/model/Team.java
+++ src/main/java/com/github/mojewski/footballleaguesimulator/model/Team.java
@@ -1,4 +1,21 @@
 package com.github.mojewski.footballleaguesimulator.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Team {
+    private Long id;
+    private String name;
+    private double budget;
+
+    private List<Player> players = new ArrayList<>();
+
+    public Team(String name, double budget) {
+        this.name = name;
+        this.budget = budget;
+    }
+
+    public Long getId() { return id; }
+    public String getName() { return name; }
+    public double getBudget() { return budget; }
 }
