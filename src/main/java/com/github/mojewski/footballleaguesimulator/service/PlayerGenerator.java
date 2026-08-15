dc1f7e8d56717fa492b17a326f9a3da5549185b8
mojewski <293508@student.pwr.edu.pl>
2026-08-15 13:40:49 +0200

    Add code to PlayerGenerator class

--- src/main/java/com/github/mojewski/footballleaguesimulator/service/PlayerGenerator.java
+++ src/main/java/com/github/mojewski/footballleaguesimulator/service/PlayerGenerator.java
@@ -1,4 +1,33 @@
 package com.github.mojewski.footballleaguesimulator.service;
 
+import com.github.mojewski.footballleaguesimulator.model.*;
+
+import java.util.concurrent.ThreadLocalRandom;
+
 public class PlayerGenerator {
+
+    public Player generateReplacement(Player retiringPlayer) {
+
+        Team team = retiringPlayer.getTeam();
+        int academyLevel = team.getAcademyRating();
+
+        int age = ThreadLocalRandom.current().nextInt(16,21);
+        Country country = retiringPlayer.getCountry();
+        Position position = retiringPlayer.getPosition();
+        int rawPotential = academyLevel + ThreadLocalRandom.current().nextInt(-10, 16);
+        int potential = Math.min(99, Math.max(30, rawPotential));
+        int rating = (int) (potential * ThreadLocalRandom.current().nextDouble(0.6, 0.76));
+
+        return new PlayerBuilder()
+                .setFirstName("New")  //TODO: NameGenerator!
+                .setLastName("Player")
+                .setAge(age)
+                .setCountry(country)
+                .setPosition(position)
+                .setPotential(potential)
+                .setRating(rating)
+                .setTeam(team)
+                .setIsRetired(false)
+                .getResult();
+    }
 }
